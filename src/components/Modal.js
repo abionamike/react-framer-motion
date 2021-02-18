4d6b54146430f3f6585f1a686d866866f8073d67
Abiona Michael <amabiona21@gmail.com>
2021-02-18 15:26:40 +0100

    animating modal 2

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,24 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const backdropVariant = {
   visible: { opacity: 1 },
   hidden: { opacity: 0 }
+}
+
+const modalVariant = {
+  hidden: { 
+    y: '-100vh', 
+    opacity: 0 
+  },
+  visible: { 
+    y: 200, 
+    opacity: 1, 
+    transition: {
+      delay: 0.5
+    } 
+  }
 }
 
 const Modal = ({ showModal, setShowModal }) => {
@@ -15,8 +29,16 @@ const Modal = ({ showModal, setShowModal }) => {
           variants={backdropVariant}
           initial='hidden'
           animate='visible'
+          exit='hidden'
         >
-
+          <motion.div className='modal'
+            variants={modalVariant}
+          >
+            <p>Want to make another pizza?</p>
+            <Link to="/">
+              <button>Start Again</button>
+            </Link>
+          </motion.div>
         </motion.div>
       )}
     </AnimatePresence>
